fc0e5e27cc8959b65a09905ff301c8f447a7397f
rwoj <rwoj@poczta.onet.pl>
2020-09-13 15:17:15 +0200

    orders created

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from "@rwojtickets/common";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
